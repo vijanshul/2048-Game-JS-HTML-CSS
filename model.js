46b54c567f102e78760180daa0816de297c169e8
Anshul Vij <vijanshul04@gmail.com>
2022-08-11 14:27:56 -0400

    Format HTML, Model Files

--- model.js
+++ model.js
@@ -2,12 +2,12 @@ import Game from "./engine/game.js";
 let initialize = false;
 
 export const setup = function(game) {
-    for(let i = 0; i < game.board.length; i++){
+    for (let i = 0; i < game.board.length; i++) {
         document.getElementById(i.toString(10)).innerHTML = game.board[i];
-        if(game.board[i] == 0){
-            $(`#${i.toString(10)}`).css("visibility", "hidden"); 
-        }else{
-            $(`#${i.toString(10)}`).css("visibility", "visible"); 
+        if (game.board[i] == 0) {
+            $(`#${i.toString(10)}`).css("visibility", "hidden");
+        } else {
+            $(`#${i.toString(10)}`).css("visibility", "visible");
             $(`#${i.toString(10)}`).css("background", "#d4af37");
             $(`#${i.toString(10)}`).css("color", "black");
             $(`#${i.toString(10)}`).css("font-size", "2vw");
@@ -15,7 +15,7 @@ export const setup = function(game) {
     }
 }
 
-export const keys = function(k, board){
+export const keys = function(k, board) {
     switch (k) {
         case '&':
             board.move('up');
@@ -33,16 +33,16 @@ export const keys = function(k, board){
 
 }
 
-export const newGame = function(game){
+export const newGame = function(game) {
     document.getElementById("score").innerHTML = "Your score: " + game.score;
-    if(initialize == false){
+    if (initialize == false) {
         initialize = true;
-        $('#reset').on("click", function(){
+        $('#reset').on("click", function() {
             game.setupNewGame();
             document.getElementById("end").innerHTML = "";
             newGame(game);
         });
-        $(document).keydown(function(e){
+        $(document).keydown(function(e) {
             var s = String.fromCharCode(e.which);
             keys(s, game);
             update(game);
@@ -52,19 +52,19 @@ export const newGame = function(game){
 
 };
 
-export const update = function(game){
+export const update = function(game) {
     setup(game);
     document.getElementById("score").innerHTML = "Your score: " + game.score;
-    if(game.over == true){
+    if (game.over == true) {
         document.getElementById("end").innerHTML = "You Lost! Score: " + game.score + ". Press Reset Game";
     }
-    if(game.won == true){
+    if (game.won == true) {
         document.getElementById("end").innerHTML = "Congrats! You can keep going :)";
 
     }
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
     let game = new Game(4);
     newGame(game);
 });
